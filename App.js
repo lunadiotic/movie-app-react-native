153f80f99cd92dc2693908743322d82e1b9be77c
IDStack <idstack@outlook.co.id>
2022-07-05 15:12:50 +0700

    error handling

--- App.js
+++ App.js
@@ -2,22 +2,22 @@ import React, { useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
 import axios from 'axios';
 
-const getPopMovies = async () => {
-	const response = await axios.get(
-		'https://api.themoviedb.org/3/movie/popular?api_key=778306490922af732ee8ce5aeb1ef02e'
-	);
-	return response.data.results;
-};
-
 const App = () => {
 	const [movie, setMovie] = useState({});
+	const [err, setErr] = useState(false);
 
 	useEffect(() => {
-		getPopMovies()
-			.then((result) => {
-				setMovie(result[0]);
-			})
-			.catch((err) => {});
+		const getPopMovies = async () => {
+			try {
+				const response = await axios.get(
+					'https://api.themoviedb.org/3/movie/popular?api_key=778306490922af732ee8ce5aeb1ef02e'
+				);
+				setMovie(response.data.results[0]);
+			} catch (err) {
+				setErr(err);
+			}
+		};
+		getPopMovies();
 	}, []);
 
 	return (
@@ -31,6 +31,7 @@ const App = () => {
 			<Text>{movie.original_title}</Text>
 			<Text>{movie.original_language}</Text>
 			<Text>{movie.release_date}</Text>
+			{err && <Text style={{ color: 'red' }}>Internal Server Error</Text>}
 		</View>
 	);
 };
