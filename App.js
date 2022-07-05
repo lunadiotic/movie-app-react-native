202a92fd9a441a71d655f08da1bd8b365e148afb
IDStack <idstack@outlook.co.id>
2022-07-05 18:07:55 +0700

    make child component screen home

--- App.js
+++ App.js
@@ -1,21 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
-import { getPopularMovies } from './services/services';
+import React from 'react';
+import { View } from 'react-native';
+import Home from './screens/Home';
 
 const App = () => {
-	const [movie, setMovie] = useState('');
-	const [err, setErr] = useState(false);
-
-	useEffect(() => {
-		getPopularMovies()
-			.then((movies) => {
-				setMovie(movies[0]);
-			})
-			.catch((err) => {
-				setErr(err);
-			});
-	}, []);
-
 	return (
 		<View
 			style={{
@@ -24,9 +11,7 @@ const App = () => {
 				alignItems: 'center',
 			}}
 		>
-			<Text>Title: {movie.original_title}</Text>
-			<Text>Release Date: {movie.release_date}</Text>
-			{err && <Text style={{ color: 'red' }}>Internal Server Error</Text>}
+			<Home />
 		</View>
 	);
 };
---
+++ screens/Home.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import { Text } from 'react-native';
+import { getPopularMovies } from '../services/services';
+
+const Home = () => {
+	const [movie, setMovie] = useState('');
+	const [err, setErr] = useState(false);
+
+	useEffect(() => {
+		getPopularMovies()
+			.then((movies) => {
+				setMovie(movies[0]);
+			})
+			.catch((err) => {
+				setErr(err);
+			});
+	}, []);
+	return (
+		<>
+			<Text>Title: {movie.original_title}</Text>
+			<Text>Release Date: {movie.release_date}</Text>
+			{err && <Text style={{ color: 'red' }}>Internal Server Error</Text>}
+		</>
+	);
+};
+
+export default Home;
